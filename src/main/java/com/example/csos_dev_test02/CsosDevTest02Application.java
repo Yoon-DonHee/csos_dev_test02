5e932fa36668a7bba22e8420b79dc564a8abeb9d
YoonDonHee <dh.yoon@sqisoft.com>
2021-07-12 15:09:59 +0900

    test

---
+++ src/main/java/com/example/csos_dev_test02/CsosDevTest02Application.java
@@ -0,0 +1,13 @@
+package com.example.csos_dev_test02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsosDevTest02Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CsosDevTest02Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/csos_dev_test02/CsosDevTest02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.csos_dev_test02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsosDevTest02ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
